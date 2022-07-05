8171719732cbb4c9f7bf8050d51cc56e96ee82ad
Nick Cruz <nickcruz920@gmail.com>
2022-07-05 09:50:33 -0400

    Initial Creation - Created an array of months

---
+++ index.js
@@ -0,0 +1,2 @@
+// Array of Months
+let months = ["January: 2022,", "February: 2022", "March: 2022", "April: 2022", "May: 2022", "June: 2022", "July: 2022", "August: 2022", "September: 2022", "October: 2022", "November: 2022", "December: 2022"];
