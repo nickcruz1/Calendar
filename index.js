f627cc5c187598403b02948b9f8bcff3803d4ca4
Nick Cruz <nickcruz920@gmail.com>
2022-07-05 09:51:32 -0400

    Added some test code
    Added some test code

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ let months = ["January: 2022,", "February: 2022", "March: 2022", "April: 2022", 
 
 // Array of Days
 let days = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
+
+// Test Variables
+let v1 = "Hey";
+let v2 = "Hi";
+let v3 = "Ni";
+
+// An array of variables
+let big = [v1,v2,v3]
+ 
+for(let i = 0; i < months.length; i++) {
+  console.log(months[i]);
+}
+
+console.log(big[0])
