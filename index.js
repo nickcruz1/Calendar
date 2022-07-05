141dcff0b526ac041657f0c4bc13ff720e93b68e
Nick Cruz <nickcruz920@gmail.com>
2022-07-05 09:50:58 -0400

    Created an array of days
    Created an array of the days of the month

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 // Array of Months
 let months = ["January: 2022,", "February: 2022", "March: 2022", "April: 2022", "May: 2022", "June: 2022", "July: 2022", "August: 2022", "September: 2022", "October: 2022", "November: 2022", "December: 2022"];
+
+// Array of Days
+let days = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
